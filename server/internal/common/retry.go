fa2939a76b01fade93240fba97c1ee4b6dcc27e4
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2024-11-09 16:07:08 +0300

    Use an universal method for making an action with retries

---
+++ server/internal/common/retry.go
@@ -0,0 +1,34 @@
+package common
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+type action func(ctx context.Context) error
+
+func ExecuteWithRetries(ctx context.Context, act action) error {
+	retryNum := 10
+	waitTime := 500 * time.Millisecond
+	var err error
+
+	for i := 0; i < retryNum; i++ {
+		if ctx.Err() != nil {
+			return nil
+		}
+
+		err = act(ctx)
+
+		waitWithContext(ctx, waitTime)
+	}
+
+	return fmt.Errorf("retries are exceeded: %w", err)
+}
+
+func waitWithContext(ctx context.Context, waitTime time.Duration) {
+	select {
+	case <-ctx.Done():
+	case <-time.After(waitTime):
+	}
+}
