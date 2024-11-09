67dfd6c3542e18c6223be4e0c740896f2b1d816c
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2024-11-09 15:36:32 +0300

    Amend the interface transmitter

--- server/internal/interfaces/broker/transmitter.go
+++ server/internal/interfaces/broker/transmitter.go
@@ -3,9 +3,10 @@ package service
 import (
 	"context"
 
-	serviceEnt "github.com/Guise322/TeleBot/server/internal/entities/service"
+	"github.com/Guise322/TeleBot/server/internal/entities/service"
 )
 
 type DataTransmitter interface {
-	StartTransmittingData(ctx context.Context) chan<- serviceEnt.OutData
+	TransmitData(ctx context.Context, data service.OutData) error
+	Close()
 }
