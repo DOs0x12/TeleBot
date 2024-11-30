b08efc25f02aa75bb3fba9f7d6abd4ffbd336bc9
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2024-11-30 19:33:32 +0300

    Add tests for the method removeOldOffcets of the committer

--- server/internal/infrastructure/broker/committer_test.go
+++ server/internal/infrastructure/broker/committer_test.go
@@ -34,3 +34,30 @@ func TestRemoveOldMessages(t *testing.T) {
 		})
 	}
 }
+
+func TestRemoveOldOffcets(t *testing.T) {
+	threshold := 1 * time.Hour
+	tests := []struct {
+		name  string
+		input time.Duration
+		want  bool
+	}{
+		{"Have an offcet", threshold, true},
+		{"Have no offcet with the equal threashold", -threshold, false},
+		{"Have no offcet with a greater threashold", -(threshold + 1*time.Millisecond), false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testOffcets := make(map[int]offcetWithTimeStamp)
+			offcetVal := 1
+			testOffcets[offcetVal] = offcetWithTimeStamp{value: 1, timeStamp: time.Now()}
+
+			removeOldOffcets(testOffcets, tt.input)
+			_, ok := testOffcets[offcetVal]
+			if tt.want != ok {
+				t.Errorf("have the offcet: %v, want: %v", ok, tt.want)
+			}
+		})
+	}
+}
