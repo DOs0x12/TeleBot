842b5fac0f4434db4065df56ab5c7d9bca4c739d
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2025-01-02 19:56:57 +0300

    Write the logic of getting an ID or a command token

--- client/token/service.go
+++ client/token/service.go
@@ -1,13 +1,20 @@
 package token
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
+	"os"
+	"path"
 
 	"github.com/thanhpk/randstr"
 )
 
+const dataFolderPath = "/usr/local/telebot"
+const dataFileName = "system_id"
+
 func GetOrCreateCommandToken(commandName string) (string, error) {
-	sysID, err := getIDFromSystem()
+	sysID, err := getOrCreateSystemID()
 	if err != nil {
 		return "", fmt.Errorf("failed to load an ID from the system: %w", err)
 	}
@@ -17,12 +24,60 @@ func GetOrCreateCommandToken(commandName string) (string, error) {
 	return comToken, nil
 }
 
-func getIDFromSystem() (string, error) {
-	return "", nil
+func getOrCreateSystemID() (string, error) {
+	folderExists, err := dataFolderExists()
+	if err != nil {
+		return "", err
+	}
+
+	if !folderExists {
+		err := os.Mkdir(dataFolderPath, os.FileMode(os.O_RDWR))
+		if err != nil {
+			return "", err
+		}
+	}
+
+	dataFilePath := path.Join(dataFolderPath, dataFileName)
+	file, err := os.OpenFile(dataFilePath, os.O_CREATE, os.FileMode(os.O_RDWR))
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	var fileData []byte
+	_, err = file.Read(fileData)
+	if err != nil {
+		return "", err
+	}
+
+	systemID := string(fileData)
+	if systemID == "" {
+		systemID = generateNewID()
+		_, err = file.WriteString(systemID)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return systemID, nil
 }
 
 func generateNewID() string {
 	tokenLength := 10
 	id := randstr.Base62(tokenLength)
 	return id
+}
+
+func dataFolderExists() (bool, error) {
+	_, err := os.Stat(dataFolderPath)
+	if err == nil {
+		return true, nil
+	}
+
+	if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	}
+
+	return false, err
 }
