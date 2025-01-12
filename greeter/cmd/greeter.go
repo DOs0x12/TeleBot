42ce5a262fa29ea9710ec285f227516c68bbb48b
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2025-01-12 17:46:35 +0300

    Update the greeter by the updated logic of the client

--- greeter/cmd/greeter.go
+++ greeter/cmd/greeter.go
@@ -6,7 +6,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/DOs0x12/TeleBot/client/broker"
+	"github.com/DOs0x12/TeleBot/client/v2/broker"
 
 	"github.com/sirupsen/logrus"
 )
@@ -16,38 +16,38 @@ func main() {
 	kafkaAddr := flag.String("conn", "kafka:9092", "The kafka connection string.")
 	flag.Parse()
 
-	commData := broker.CommandData{Name: "hello", Description: "Say hello to the bot"}
-	r, err := broker.NewReceiver(*kafkaAddr, commData.Name)
+	commData := broker.BrokerCommandData{Name: "hello", Description: "Say hello to the bot"}
+
+	serviceName := "greeter"
+	br, err := broker.NewKafkaBroker(ctx, *kafkaAddr, serviceName)
 	if err != nil {
 		logrus.Error(err)
 
 		return
 	}
 
-	s := broker.NewSender(*kafkaAddr)
-
-	err = s.RegisterCommand(ctx, commData)
+	err = br.RegisterCommand(ctx, commData, serviceName)
 	if err != nil {
 		logrus.Error("Failed to register a command: ", err)
 
 		return
 	}
 
-	inDataChan := r.StartGetData(ctx)
+	inDataChan := br.StartGetData(ctx)
 
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case inData := <-inDataChan:
-			outData := broker.BotData{ChatID: inData.ChatID, Value: "Hello there!"}
-			err := s.SendData(ctx, outData)
+			outData := broker.BrokerData{ChatID: inData.ChatID, Value: "Hello there!"}
+			err := br.SendData(ctx, outData)
 			if err != nil {
 				logrus.Error("Failed to send data: ", err)
 
 				continue
 			}
-			err = r.Commit(ctx, inData.MessageUuid)
+			err = br.Commit(ctx, inData.MessageUuid)
 			if err != nil {
 				logrus.Error("Failed to commit data: ", err)
 
