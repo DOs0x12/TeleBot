540b7b3ec6bd5f439ae96ebdb988fdf22e7fe465
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2025-01-18 18:32:43 +0300

    Move the logic of processing the incoming from broker data to a method

--- server/internal/app/service.go
+++ server/internal/app/service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	botEnt "github.com/DOs0x12/TeleBot/server/v2/internal/entities/bot"
+	"github.com/DOs0x12/TeleBot/server/v2/internal/entities/broker"
 	botInterf "github.com/DOs0x12/TeleBot/server/v2/internal/interfaces/bot"
 	brokerInterf "github.com/DOs0x12/TeleBot/server/v2/internal/interfaces/broker"
 	storInterf "github.com/DOs0x12/TeleBot/server/v2/internal/interfaces/storage"
@@ -58,42 +59,7 @@ func Process(ctx context.Context,
 
 			return nil
 		case fromBrokerData := <-fromBrokerDataChan:
-			go func() {
-				if fromBrokerData.IsCommand {
-					comm, err := command(fromBrokerData.Value)
-					if err != nil {
-						logrus.Error("Failed to get a command form a broker message: ", err)
-
-						return
-					}
-
-					err = registerBotCommand(ctx, comm, botConf)
-					if err != nil {
-						logrus.Error("Failed to register a command in the bot: ", err)
-
-						return
-					}
-
-					return
-				}
-
-				toBotData, err := castFromBrokerData(fromBrokerData)
-				if err != nil {
-					logrus.Error("Failed to cast data for sending it to the bot: ", err)
-				}
-
-				err = botConf.BotWorker.SendMessage(ctx, toBotData.Value, toBotData.ChatID)
-				if err != nil {
-					logrus.Error("Dailed to send a message to the bot: ", err)
-
-					return
-				}
-
-				err = msgBroker.Commit(ctx, fromBrokerData.MsgUuid)
-				if err != nil {
-					logrus.WithField("messageUuid", err).Error("Failed to commit the message with UUID: ", err)
-				}
-			}()
+			go processFromBrokerData(ctx, fromBrokerData, botConf, msgBroker)
 		case fromBotData := <-fromBotDataChan:
 			toBrokerData, err := processFromBotData(fromBotData, *botConf.BotCommands)
 			if err != nil {
@@ -109,5 +75,45 @@ func Process(ctx context.Context,
 				}
 			}()
 		}
+	}
+}
+
+func processFromBrokerData(ctx context.Context,
+	fromBrokerData broker.DataFrom,
+	botConf BotConf,
+	msgBroker brokerInterf.MessageBroker) {
+	if fromBrokerData.IsCommand {
+		comm, err := command(fromBrokerData.Value)
+		if err != nil {
+			logrus.Error("Failed to get a command form a broker message: ", err)
+
+			return
+		}
+
+		err = registerBotCommand(ctx, comm, botConf)
+		if err != nil {
+			logrus.Error("Failed to register a command in the bot: ", err)
+
+			return
+		}
+
+		return
+	}
+
+	toBotData, err := castFromBrokerData(fromBrokerData)
+	if err != nil {
+		logrus.Error("Failed to cast data for sending it to the bot: ", err)
+	}
+
+	err = botConf.BotWorker.SendMessage(ctx, toBotData.Value, toBotData.ChatID)
+	if err != nil {
+		logrus.Error("Dailed to send a message to the bot: ", err)
+
+		return
+	}
+
+	err = msgBroker.Commit(ctx, fromBrokerData.MsgUuid)
+	if err != nil {
+		logrus.WithField("messageUuid", err).Error("Failed to commit the message with UUID: ", err)
 	}
 }
