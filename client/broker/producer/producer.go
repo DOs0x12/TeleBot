99aa844b75aa46510672d0e32a50e9d92be95415
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2025-05-31 18:51:11 +0300

    Up version of client to v3 in go.mod

--- client/broker/producer/producer.go
+++ client/broker/producer/producer.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/DOs0x12/TeleBot/client/v2/broker/topic"
+	"github.com/DOs0x12/TeleBot/client/v3/broker/topic"
 	"github.com/DOs0x12/TeleBot/server/v3/system"
 	"github.com/google/uuid"
 	"github.com/segmentio/kafka-go"
